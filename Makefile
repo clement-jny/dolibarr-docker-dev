cf8f45b33ddcf426f1cfd201a6d7bc05bc6aa3f5
Clément Jny. <clj@ckd-industrie.com>
2025-04-28 22:31:04 +0200

    feat: create script to up/down database & create Makefile

---
+++ Makefile
@@ -0,0 +1,5 @@
+# Database part
+up_database:
+	bash scripts/db/up_database.sh
+down_database:
+	bash scripts/db/down_database.sh
---
+++ scripts/db/down_database.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+COMPOSE_FILE="composes/db/compose.yml"
+
+if [ ! -f "${COMPOSE_FILE}" ]; then
+  echo "❌ Compose file ${COMPOSE_FILE} not found!"
+  exit 1
+fi
+
+echo "🛑 Stopping database..."
+docker compose -f "${COMPOSE_FILE}" down
