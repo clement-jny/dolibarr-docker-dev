d25ca23573d974240021f2529cd31b95d133a3c8
Clément Jny. <clj@ckd-industrie.com>
2025-08-06 15:57:12 +0200

    feat: create pre-configured conf file for installation

--- scripts/dolibarr/get_dolibarr.sh
+++ scripts/dolibarr/get_dolibarr.sh
@@ -36,6 +36,63 @@ fi
 # mkdir -p documents
 
 
+# Create a pre-configured conf.php file
+echo "⬇️  Creating pre-configured conf.php for Dolibarr ${VERSION}..."
+cat > htdocs/conf/conf.php <<CONF_EOF
+<?php
+//
+// File pre-configured for Dolibarr ${VERSION} on $(date '+%Y-%m-%d')
+//
+// Take a look at conf.php.example file for an example of conf.php file
+// and explanations for all possibles parameters.
+//
+\$dolibarr_main_url_root='http://dlb${CLEAN_VERSION}.localhost';
+\$dolibarr_main_document_root="/var/www/html";
+\$dolibarr_main_url_root_alt='/custom';
+\$dolibarr_main_document_root_alt="/var/www/html/custom";
+\$dolibarr_main_data_root="/var/www/documents";
+\$dolibarr_main_db_host='mariadb';
+\$dolibarr_main_db_port='3306';
+\$dolibarr_main_db_name='dolibarr_${CLEAN_VERSION}';
+\$dolibarr_main_db_prefix='llx_';
+\$dolibarr_main_db_user='dolibarr_user_${CLEAN_VERSION}';
+\$dolibarr_main_db_pass='dolibarr_password_${CLEAN_VERSION}';
+\$dolibarr_main_db_type='mysqli';
+\$dolibarr_main_db_character_set='utf8';
+\$dolibarr_main_db_collation='utf8_unicode_ci';
+// Authentication settings
+\$dolibarr_main_authentication='dolibarr';
+
+//\$dolibarr_main_demo='autologin,autopass';
+// Security settings
+\$dolibarr_main_prod='0';
+\$dolibarr_main_force_https='0';
+\$dolibarr_main_restrict_os_commands='mariadb-dump, mariadb, mysqldump, mysql, pg_dump, pg_restore, clamdscan, clamdscan.exe';
+\$dolibarr_nocsrfcheck='0';
+\$dolibarr_main_instance_unique_id='$(openssl rand -hex 16)';
+\$dolibarr_mailing_limit_sendbyweb='0';
+\$dolibarr_mailing_limit_sendbycli='0';
+
+//\$dolibarr_lib_FPDF_PATH="";
+//\$dolibarr_lib_TCPDF_PATH="";
+//\$dolibarr_lib_FPDI_PATH="";
+//\$dolibarr_lib_TCPDI_PATH="";
+//\$dolibarr_lib_GEOIP_PATH="";
+//\$dolibarr_lib_NUSOAP_PATH="";
+//\$dolibarr_lib_ODTPHP_PATH="";
+//\$dolibarr_lib_ODTPHP_PATHTOPCLZIP="";
+//\$dolibarr_js_CKEDITOR='';
+//\$dolibarr_js_JQUERY='';
+//\$dolibarr_js_JQUERY_UI='';
+
+//\$dolibarr_font_DOL_DEFAULT_TTF='';
+//\$dolibarr_font_DOL_DEFAULT_TTF_BOLD='';
+\$dolibarr_main_distrib='standard';
+CONF_EOF
+
+echo "✅ Pre-configured conf.php created."
+
+
 # Create the compose file for the version
 echo "⬇️  Creating compose file for Dolibarr ${VERSION}..."
 
